aa603ed97620a18be0928650fb622fc36ff4f7ff
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-04-25 21:57:07 +0530

    variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+	// var name = "golang"
+	var isAdult bool = true
+	var age int = 30
+	fmt.Println(isAdult)
+	fmt.Println(age)
+
+	var price float32
+	price = 50.5
+	//shorthand notation
+	// name := "golang"
+	fmt.Println(price)
+}
