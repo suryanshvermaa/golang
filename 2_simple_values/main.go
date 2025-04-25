8c9a25a089c16aed2a9b67211d49130464a841df
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-04-25 21:57:07 +0530

    variables

---
+++ 2_simple_values/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//int
+	fmt.Println(1 + 1)
+	//string
+	fmt.Println("Hello golang")
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+	//floats
+	fmt.Println(10.5)
+	fmt.Println(7.3 / 4.3)
+}
