39fe1b90d294054282dd54b86b217e3dfdf624ad
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-04-25 22:29:54 +0530

    for

---
+++ 5_for/for.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// for-> only construct in go for looping
+func main() {
+	//while loop
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// //infinite loop
+	// for {
+	// 	println("1")
+	// }
+
+	//classic for loop
+	for i := 0; i <= 3; i++ {
+		if i == 2 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	// 1.22 range
+	for i := range 3 { //->0,1,2
+		fmt.Println(i)
+	}
+}
